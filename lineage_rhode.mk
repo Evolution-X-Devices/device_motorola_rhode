d0df41bb2f94be07e2993853cd65d773127dffd1
Deivid21 <david.parra.ignacio@gmail.com>
2025-07-18 00:13:40 -0400

    fixup! rhode: Update to T2SR33.72-22-4-13
    Change-Id: I4bb9c42da0493829d3ff2ef6fe71129f789b1aae

--- lineage_rhode.mk
+++ lineage_rhode.mk
@@ -23,6 +23,6 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="rhode_g-user 13 T2SR33.72-22-4-13 6b3c4b-075d74 release-keys" \
-    BuildFingerprint=motorola/rhode_g/rhode:13/T2SR33.72-22-4-13/6b3c4b-075d74:user/release-keys \
+    BuildDesc="rhode_g-user 11 T2SR33.72-22-4-13 6b3c4b release-keys" \
+    BuildFingerprint=motorola/rhode_g/rhode:11/T2SR33.72-22-4-13/6b3c4b:user/release-keys \
     DeviceProduct=rhode_g
